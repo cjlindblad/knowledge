b679aaadf13f654f299257c568f00658d13afc69
cj lindblad <carljohan.lindblad@gmail.com>
2020-05-02 16:11:49 +0200

    fetch knowledge from local sqlite db

--- src/interface/knowledge_service.py
+++ src/interface/knowledge_service.py
@@ -1,12 +1,28 @@
+import sqlite3
+from datetime import datetime
+
 from knowledge_item import KnowledgeItem
 
 class KnowledgeService:
     def list_knowledge(self):
-        knowledge = [
-            KnowledgeItem('2020-01-11', 'Learned a new unix command', 'sdgj khdk hsadg /n dsg ladhsg lsdhglds kg/n dsjglk sdahglkads g/n djlg adhslg dh/n'),
-            KnowledgeItem('2020-02-13', 'Prototype with SQLite', 'hai lhfasil gah/n asfhl safhlkasf iashf /n ashfilas fhilas fh/n ashfli ashfilas f'),
-            KnowledgeItem('2019-05-23', 'VirtualBox init', 'dsgdslhig sdg/n sdigsdi lgudsilgds ug/n sdjgds lgjsdl gds/n sdgiohsdo gdhsiog s'),
-            KnowledgeItem('2019-07-01', 'CSS works in mysterious ways', 'sd jfiladsj filadsf /n sdufi sdaufidsa fuidsa f/n sdufop asdufop asdufopsd f/n sadufidso fuiods fuas/n sdufi sdufio sdauf'),
-            KnowledgeItem('2020-03-03', 'Markdown 101', 'hdis fhdslk f/n sfhija sfhidlsahklghsghs /n adsjfi dsfids fiodsa f/n dsufi odasufopads fudosp f/n dsaif opdsifo;ds f')
-        ]
+        connection = sqlite3.connect('../../db/knowledge.db')
+        cursor = connection.cursor()
+
+        query = '''
+        SELECT * FROM knowledge_item
+        ORDER BY created_ts DESC;
+        '''
+
+        knowledge = []
+
+        result = cursor.execute(query)
+        for id, created, title, content in result:
+            knowledge.append(KnowledgeItem(
+                datetime.utcfromtimestamp(created).strftime('%Y-%m-%d'),
+                title,
+                content
+            ))
+
+        connection.close()
+
         return knowledge
