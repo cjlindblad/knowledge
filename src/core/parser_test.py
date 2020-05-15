3528cb57623663d6124f20453a0181b98db0b988
cj lindblad <carljohan.lindblad@gmail.com>
2020-05-15 21:50:12 +0200

    added test for parsing text representation

--- src/core/parser_test.py
+++ src/core/parser_test.py
@@ -3,22 +3,37 @@ from src.core.parser import knowledge_item_to_text, text_to_knowledge_item
 from src.core.knowledge_item import KnowledgeItem
 
 
-class Test_TestParser(unittest.TestCase):
-    def test_parses_plain_knowledge_item(self):
-        title = 'test title'
-        category = 'test category'
-        content = 'test content'
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        self.fields = {
+            'title': 'test title',
+            'category': 'test category',
+            'content': 'test content'
+        }
 
+    def __text_representation(self, fields):
+        return f'title: {fields["title"]}\ncategory: {fields["category"]}\ncontent: {fields["content"]}\n'
+
+    def test_parses_plain_knowledge_item(self):
         item = KnowledgeItem()
-        item.title = title
-        item.category = category
-        item.content = content
+        item.title = self.fields['title']
+        item.category = self.fields['category']
+        item.content = self.fields['content']
 
         text = knowledge_item_to_text(item)
 
-        expected_result = f'title: {title}\ncategory: {category}\ncontent: {content}\n'
+        expected_result = self.__text_representation(self.fields)
 
         self.assertEqual(text, expected_result)
+
+    def test_parses_plain_textual_representation(self):
+        text = self.__text_representation(self.fields)
+
+        item = text_to_knowledge_item(text)
+
+        self.assertEqual(self.fields['title'], item.title)
+        self.assertEqual(self.fields['category'], item.category)
+        self.assertEqual(self.fields['content'], item.content)
 
 
 if __name__ == '__main__':
