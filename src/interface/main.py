24cc10d0ba5b10d3632e849fe118215a03e7bb25
cj lindblad <carljohan.lindblad@gmail.com>
2020-05-26 12:38:08 +0200

    Moved lots of interface data members to self.

--- src/interface/main.py
+++ src/interface/main.py
@@ -18,10 +18,28 @@ class Display:
     def __init__(self):
         self.__setup()
 
+        # members
+        self.knowledge_repo = KnowledgeRepository()
+        self.k = 0
+        self.screen_state = ScreenState.LIST_ACTIVE
+        self.search_term = ''
+        self.data = []
+
+        # constants
+        self.LIST_TOP_MARGIN = 1
+        self.WIN_HEIGHT, self.WIN_WIDTH = self.stdscr.getmaxyx()
+        self.STATUS_BAR_HEIGHT = 1
+        self.LIST_START = self.LIST_TOP_MARGIN
+        self.LIST_END = self.WIN_HEIGHT - 1 - self.STATUS_BAR_HEIGHT
+        self.PAGE_LENGTH = self.WIN_HEIGHT - self.LIST_TOP_MARGIN - self.STATUS_BAR_HEIGHT
+
+        self.navigator = ListNavigator(0, self.PAGE_LENGTH)
+
     def __del__(self):
         self.__teardown()
 
     def __setup(self):
+        # sets up curses stuff
         self.stdscr = curses.initscr()
 
         curses.noecho()
@@ -34,81 +52,68 @@ class Display:
         self.stdscr.refresh()
 
     def __teardown(self):
+        # tears down curses stuff
         curses.nocbreak()
         self.stdscr.keypad(False)
         curses.echo()
 
         curses.endwin()
 
+    def navigate(self):
+        pass
+
     def draw_screen(self):
         """This one screams for refactoring,
         but I'm going to play around just for a bit longer"""
 
-        # members
-        knowledge_repo = KnowledgeRepository()
-        k = 0
-        screen_state = ScreenState.LIST_ACTIVE
-        search_term = ''
-        data = []
-
-        # constants
-        LIST_TOP_MARGIN = 1
-        WIN_HEIGHT, WIN_WIDTH = self.stdscr.getmaxyx()
-        STATUS_BAR_HEIGHT = 1
-        LIST_START = LIST_TOP_MARGIN
-        LIST_END = WIN_HEIGHT - 1 - STATUS_BAR_HEIGHT
-        PAGE_LENGTH = WIN_HEIGHT - LIST_TOP_MARGIN - STATUS_BAR_HEIGHT
-
-        navigator = ListNavigator(0, PAGE_LENGTH)
-
         # main loop
         while (True):
             # key listeners for list screen state
-            if screen_state == ScreenState.LIST_ACTIVE or screen_state == ScreenState.LIST_ARCHIVED:
-                if k == curses.KEY_UP:
-                    navigator.prev()
-                if k == curses.KEY_DOWN:
-                    navigator.next()
-                if k == curses.KEY_RIGHT:
-                    navigator.next_page()
-                if k == curses.KEY_LEFT:
-                    navigator.prev_page()
-                if k in (curses.KEY_ENTER, 10, 13):
-                    if len(data) > 0:
-                        screen_state = ScreenState.ITEM
-                if k and curses.ascii.isprint(chr(k)):
-                    search_term = search_term + chr(k)
-                if k in (curses.KEY_BACKSPACE, 127):
-                    search_term = search_term[:-1]
-                if curses.keyname(k) == b'^T':
-                    if screen_state == ScreenState.LIST_ACTIVE:
-                        screen_state = ScreenState.LIST_ARCHIVED
-                    elif screen_state == ScreenState.LIST_ARCHIVED:
-                        screen_state = ScreenState.LIST_ACTIVE
-                if curses.keyname(k) == b'^D':
-                    if len(data) == 0:
+            if self.screen_state == ScreenState.LIST_ACTIVE or self.screen_state == ScreenState.LIST_ARCHIVED:
+                if self.k == curses.KEY_UP:
+                    self.navigator.prev()
+                if self.k == curses.KEY_DOWN:
+                    self.navigator.next()
+                if self.k == curses.KEY_RIGHT:
+                    self.navigator.next_page()
+                if self.k == curses.KEY_LEFT:
+                    self.navigator.prev_page()
+                if self.k in (curses.KEY_ENTER, 10, 13):
+                    if len(self.data) > 0:
+                        self.screen_state = ScreenState.ITEM
+                if self.k and curses.ascii.isprint(chr(self.k)):
+                    self.search_term = self.search_term + chr(self.k)
+                if self.k in (curses.KEY_BACKSPACE, 127):
+                    self.search_term = self.search_term[:-1]
+                if curses.keyname(self.k) == b'^T':
+                    if self.screen_state == ScreenState.LIST_ACTIVE:
+                        self.screen_state = ScreenState.LIST_ARCHIVED
+                    elif self.screen_state == ScreenState.LIST_ARCHIVED:
+                        self.screen_state = ScreenState.LIST_ACTIVE
+                if curses.keyname(self.k) == b'^D':
+                    if len(self.data) == 0:
                         pass
                     else:
-                        selected_item = data[navigator.selected]
-                        if screen_state == ScreenState.LIST_ACTIVE:
-                            knowledge_repo.archive(selected_item.id)
-                        elif screen_state == ScreenState.LIST_ARCHIVED:
-                            knowledge_repo.delete(selected_item.id)
-                if curses.keyname(k) == b'^A':
+                        selected_item = self.data[self.navigator.selected]
+                        if self.screen_state == ScreenState.LIST_ACTIVE:
+                            self.knowledge_repo.archive(selected_item.id)
+                        elif self.screen_state == ScreenState.LIST_ARCHIVED:
+                            self.knowledge_repo.delete(selected_item.id)
+                if curses.keyname(self.k) == b'^A':
                     self.__teardown()
                     text = get_text_from_editor()
                     self.__setup()
                     new_item = Parser.text_to_knowledge_item(text)
-                    knowledge_repo.add(new_item)
-                if curses.keyname(k) == b'^R':
-                    if screen_state == ScreenState.LIST_ARCHIVED:
-                        selected_item = data[navigator.selected]
-                        knowledge_repo.restore(selected_item.id)
-                if curses.keyname(k) == b'^E':
-                    if len(data) == 0:
+                    self.knowledge_repo.add(new_item)
+                if curses.keyname(self.k) == b'^R':
+                    if self.screen_state == ScreenState.LIST_ARCHIVED:
+                        selected_item = self.data[self.navigator.selected]
+                        self.knowledge_repo.restore(selected_item.id)
+                if curses.keyname(self.k) == b'^E':
+                    if len(self.data) == 0:
                         pass
                     else:
-                        selected_item = data[navigator.selected]
+                        selected_item = self.data[self.navigator.selected]
                         self.__teardown()
                         text = get_text_from_editor(
                             Parser.knowledge_item_to_text(selected_item))
@@ -117,56 +122,57 @@ class Display:
                         selected_item.title = new_item.title
                         selected_item.category = new_item.category
                         selected_item.content = new_item.content
-                        knowledge_repo.update(selected_item)
+                        self.knowledge_repo.update(selected_item)
 
             # key listeners for item screen state
-            if screen_state == ScreenState.ITEM:
-                if k == ord('b'):
-                    screen_state = ScreenState.LIST_ACTIVE
+            if self.screen_state == ScreenState.ITEM:
+                if self.k == ord('b'):
+                    self.screen_state = ScreenState.LIST_ACTIVE
 
             # ask for data
-            if screen_state == ScreenState.LIST_ACTIVE:
-                data = knowledge_repo.list(search_term)
-            elif screen_state == ScreenState.LIST_ARCHIVED:
-                data = knowledge_repo.list_archived()
+            if self.screen_state == ScreenState.LIST_ACTIVE:
+                self.data = self.knowledge_repo.list(self.search_term)
+            elif self.screen_state == ScreenState.LIST_ARCHIVED:
+                self.data = self.knowledge_repo.list_archived()
 
             # update navigator
-            navigator.set_size(len(data))
+            self.navigator.set_size(len(self.data))
 
             # clear screen
             self.stdscr.clear()
 
             # render functions
-            if screen_state == ScreenState.LIST_ACTIVE or screen_state == ScreenState.LIST_ARCHIVED:
+            if self.screen_state == ScreenState.LIST_ACTIVE or self.screen_state == ScreenState.LIST_ARCHIVED:
                 # render list
-                if navigator.selected != -1:
-                    for i, item in enumerate(data[navigator.start:navigator.stop + 1]):
+                if self.navigator.selected != -1:
+                    for i, item in enumerate(self.data[self.navigator.start:self.navigator.stop + 1]):
                         attribute = curses.A_NORMAL
-                        if navigator.selected % PAGE_LENGTH == i:
+                        if self.navigator.selected % self.PAGE_LENGTH == i:
                             attribute = curses.A_REVERSE
-                        y_position = i + LIST_TOP_MARGIN
-                        if y_position >= LIST_START and y_position <= LIST_END:
-                            self.stdscr.addstr(LIST_TOP_MARGIN + i, 0,
+                        y_position = i + self.LIST_TOP_MARGIN
+                        if y_position >= self.LIST_START and y_position <= self.LIST_END:
+                            self.stdscr.addstr(self.LIST_TOP_MARGIN + i, 0,
                                                f'{item.category and item.category.upper() or "N/A"} - {item.title} ({item.created})', attribute)
                 # render prompt
-                self.stdscr.addstr(0, 0, f'> {search_term}')
-            elif screen_state == ScreenState.ITEM:
+                self.stdscr.addstr(0, 0, f'> {self.search_term}')
+            elif self.screen_state == ScreenState.ITEM:
                 # render item content
-                item = data[navigator.selected]
-                self.stdscr.addstr(0, 0, Text.format(item.content, WIN_WIDTH))
+                item = self.data[self.navigator.selected]
+                self.stdscr.addstr(0, 0, Text.format(
+                    item.content, self.WIN_WIDTH))
 
             # render status bar
-            status_text = f'Page {navigator.current_page} / {navigator.total_pages}'
-            if screen_state == ScreenState.LIST_ARCHIVED:
+            status_text = f'Page {self.navigator.current_page} / {self.navigator.total_pages}'
+            if self.screen_state == ScreenState.LIST_ARCHIVED:
                 status_text = f'{status_text} (Archived)'
-            self.stdscr.addstr(WIN_HEIGHT - 1, 0,
+            self.stdscr.addstr(self.WIN_HEIGHT - 1, 0,
                                status_text)
 
             # paint
             self.stdscr.refresh()
 
             # wait for input
-            k = self.stdscr.getch()
+            self.k = self.stdscr.getch()
 
 
 def main():
